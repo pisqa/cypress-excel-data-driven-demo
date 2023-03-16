d10997254b4aa748600553681f80dbb6622d6bc6
pisqa <pauligoe10@hotmail.com>
2023-03-16 17:52:32 +0100

    latest readme

--- README.md
+++ README.md
@@ -10,11 +10,30 @@ Test CSS selectors
 
 
 
-## Implementation
-Test cases are defined in /cypress/fixtures/testSuite.xlsx.  
+## Test Plan
+### Xpath Selectors
+
+The objective is to verify that the SUT returns the correct results for various XPath expressions. We cover both copy-paste of the XML and XML file upload, and also "Include 'XML Item Type' in output" switched on/off.  
+The test XML is at [/cypress/fixtures/testXpath.xml](/cypress/fixtures/testXpath.xml)  
+Test cases are defined in /cypress/fixtures/testSuite.xlsx.
+15 test cases are defined, which is by no means comprehensive coverage, but sufficient for demo purposes.
 
 ![My Image](/images/excel-xpath.JPG)
 
+
+## Implementation Notes
+
+
+We use [SheetJS](https://www.npmjs.com/package/xlsx) to convert the excel test suite to json. We code this in a before:run event listener in  [/cypress.config.js](/cypress.config.js). Note we enable [experimentalInteractiveRunEvents](https://docs.cypress.io/guides/references/experiments) to permit execution in interactive mode (still experimental as of v12.8).  
+Dynamic tests are generated from the the JSON fixture file as described in [Dynamic Tests From Cypress Fixture](https://glebbahmutov.com/blog/dynamic-tests-from-fixture/). 
+For example, in [/cypress/e2e/jsoupCss.cy.js](/cypress/e2e/jsoupCss.cy.js):  
+```
+import { testSuite } from '../fixtures/cssTestSuite.json'
+:
+:
+testSuite.forEach((tc, k) => {
+
+```
 
 
 npm install
--- cypress/e2e/freeformatterXpath.cy.js
+++ cypress/e2e/freeformatterXpath.cy.js
@@ -6,32 +6,30 @@ describe('freeformatter xpath data-driven tests', () => {
 
   beforeEach(() => {
 
-    cy.fixture('testXpath.xml').as('testXpathXml');
+    cy.fixture('testXpath.xml').as('testXpathXml')
 
     //use session to preserve cookies, only have to interact with the popup for first test
     cy.session('cookieSession', () => {
-      cy.visit(ffxpUrl);
+      cy.visit(ffxpUrl)
 
       //wait for cookies popup
-      cy.get('button').contains('Manage Options', { timeout: 5000 }).should('be.visible').click();
-      cy.get('button').contains('Confirm Choices').click();
-
+      cy.get('button').contains('Manage Options', { timeout: 5000 }).should('be.visible').click()
+      cy.get('button').contains('Confirm Choices').click()
     })
+
     //need to revisit because cy.session clears the page finally
-    cy.visit(ffxpUrl);
-    cy.get('.page-title-box').invoke('text').should('contain', 'XPath Tester - Evaluator');
+    cy.visit(ffxpUrl)
+    cy.get('.page-title-box').invoke('text').should('contain', 'XPath Tester - Evaluator')
 
     //delay between each request to avoid bot detection
     cy.wait(10000)
-
-
   })
 
   testSuite.forEach((tc, k) => {
 
     it(`test case ${k + 1}: ${tc.title}`, function () {
 
-      const testXml = this.testXpathXml;
+      const testXml = this.testXpathXml
 
       //check xml load option
       if (tc.copyPaste) {
@@ -44,24 +42,24 @@ describe('freeformatter xpath data-driven tests', () => {
 
       //check the Include 'XML Item Type' in output option
       if (tc.includeXmlItemType) {
-        cy.get('#includeItemType').check();
+        cy.get('#includeItemType').check()
       } else {
-        cy.get('#includeItemType').uncheck();
+        cy.get('#includeItemType').uncheck()
       }
 
       //enter the test XPath in the XPath expression text box
       cy.get('input[id=xpathExpression]').type(tc.xPath)
-      cy.get('#formatBtn').click();
+      cy.get('#formatBtn').click()
 
       //wait for result
-      cy.get('h2').contains('XPath Result', { timeout: 10000 }).should('be.visible');
+      cy.get('h2').contains('XPath Result', { timeout: 10000 }).should('be.visible')
       cy.get('code.language-xml').eq(0).invoke('text').then((result) => {
-        var actual, expected;
+        var actual, expected
 
         //strip any linebreaks before verification
-        actual = result.replace(/\n|\r/g, "");
-        expected = tc.expectedResult.toString().replace(/\n|\r/g, "");
-        expect(actual).to.eq(expected);
+        actual = result.replace(/\n|\r/g, "")
+        expected = tc.expectedResult.toString().replace(/\n|\r/g, "")
+        expect(actual).to.eq(expected)
       })
     })
   })
